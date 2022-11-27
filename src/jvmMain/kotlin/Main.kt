14f4ea0a4a321c860be42596370a71ab3d244c95
bjroden <brianjaredroden@gmail.com>
2022-11-26 18:33:16 -0600

    Basic plot template

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -17,20 +17,62 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import org.knowm.xchart.XChartPanel
+import org.knowm.xchart.XYChart
 import org.knowm.xchart.XYChartBuilder
+import org.knowm.xchart.XYSeries
+import org.knowm.xchart.style.Styler
+import org.knowm.xchart.style.markers.SeriesMarkers
+import kotlin.random.Random
+
+val baseLineCoords = arrayOf(0.0, 1.0).toDoubleArray()
+
+fun getRandomString(length: Int) : String {
+    val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+    return (1..length)
+        .map { allowedChars.random() }
+        .joinToString("")
+}
+
+fun makeChart(): XYChart {
+    val chart = XYChartBuilder().theme(Styler.ChartTheme.GGPlot2).build()
+    chart.styler.defaultSeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Scatter
+    chart.styler.markerSize = 6
+    val baseLine = chart.addSeries("Expected values", baseLineCoords, baseLineCoords)
+    baseLine.xySeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Line
+    baseLine.marker = SeriesMarkers.NONE
+    return chart
+}
+
+fun XYChart.reset() {
+    this.seriesMap.clear()
+    val baseLine = this.addSeries("Expected values", baseLineCoords, baseLineCoords)
+    baseLine.xySeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Line
+    baseLine.marker = SeriesMarkers.NONE
+}
+
+fun randomCoord() = Pair(Random.nextDouble(1.0), Random.nextDouble(1.0))
+
+fun coordNearLine(): Pair<Double, Double> {
+    val baseCoord = Random.nextDouble(1.0)
+    val xEpsilon = Random.nextDouble(0.05)
+    val yEpsilon = Random.nextDouble(0.05)
+    val x = baseCoord - xEpsilon
+    val y = baseCoord - yEpsilon
+    return Pair(x, y)
+}
 
 @Composable
 @Preview
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
     var count = remember { mutableStateOf(0) }
-    var chart = remember { mutableStateOf(XYChartBuilder().build()) }
+    var chart = remember { mutableStateOf(makeChart()) }
 
     MaterialTheme {
         Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
             Button(
                 modifier = Modifier.align(Alignment.CenterHorizontally),
-                onClick = {count.value++}
+                onClick = { count.value++ }
             ) {
                 Text(if (count.value == 0) text else "Clicked ${count.value}!")
             }
@@ -44,24 +86,28 @@ fun App() {
             Spacer(modifier = Modifier.height(20.dp))
             Button(modifier = Modifier.align(Alignment.CenterHorizontally),
                 onClick = {
-                    chart.value.addSeries("a", DoubleArray(1) { 2.0 }, DoubleArray(1) { 5.0 })
+                    val pairs = (1..5).map { randomCoord() }
+                    val xs = pairs.map { it.first }.toDoubleArray()
+                    val ys = pairs.map { it.second }.toDoubleArray()
+                    chart.value.addSeries(getRandomString(5), xs, ys)
                 }
             ) {
-                Text("Add a")
+                Text("Add random coordinates")
             }
             Button(modifier = Modifier.align(Alignment.CenterHorizontally),
                 onClick = {
-                    chart.value.addSeries("b", DoubleArray(1) { 3.0 }, DoubleArray(1) { 8.0 })
+                    val pairs = (1..5).map { coordNearLine() }
+                    val xs = pairs.map { it.first }.toDoubleArray()
+                    val ys = pairs.map { it.second }.toDoubleArray()
+                    chart.value.addSeries(getRandomString(5), xs, ys)
                 }
             ) {
-                Text("Add b")
+                Text("Add coordinates near line")
             }
             Button(modifier = Modifier.align(Alignment.CenterHorizontally),
-                onClick = {
-                    chart.value.addSeries("c", DoubleArray(1) { 5.0 }, DoubleArray(1) { 10.0 })
-                }
+                onClick = { chart.value.reset() }
             ) {
-                Text("Add c")
+                Text("Clear data")
             }
         }
     }
